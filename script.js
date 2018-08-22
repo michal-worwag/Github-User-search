46918f10b6170924d6d5a487315eff80516bc821
Michał Worwąg <michalwo@gmail.com>
2018-08-22 20:08:04 +0200

    Modify script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ class UsersList extends React.Component {
     get users() {
       return this.props.users.map(user => <User key={user.id} user={user}/>);
     }
-  
+
     render() {
       return (
         <div>
